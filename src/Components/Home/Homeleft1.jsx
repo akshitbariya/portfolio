d328d12672960022b7088b339144012e07e2a6d7
akshitbariya <70649730+akshitbariya@users.noreply.github.com>
2023-12-06 22:53:39 +0530

    Update Homeleft1.jsx

--- src/Components/Home/Homeleft1.jsx
+++ src/Components/Home/Homeleft1.jsx
@@ -7,15 +7,18 @@ const Homeleft1 = () => {
        <span className='home-left1-heading'>Hello 👋</span>
        {/* <br/> */}
        <p className='home-left1-p'>
-       I am Akshit Bariya, a final year undergraduate student at NIT Surat. 
+       I am Akshit Bariya, graduated from NIT Surat. 
       <br/>
       <br/>
       {/* <br style={lightBlue}/> */}
-      I'm a React developer, and keep interest in developing attractive 
-      UI and improving UX. My skill set comprises HTML, CSS, Javascript 
-      and ReactJS.<br/>
+     I am skilled Next developer with a proven track record of crafting 
+      visually appealing user interfaces (UIs) and enhancing user experiences (UXs).
+           My expertise in HTML, CSS, JavaScript, ReactJS, and NextJS enables me to deliver robust and engaging web applications.
+<br/>
       <br/>
-      Apart from this, I have working knowledge of Python and MySQL, and open to learn newer technologies.
+ 
+         Additionally, my technical proficiency in SQL, Python, and various Python libraries provides me the versatility
+         to handle diverse project requirements.
        </p>    
 
     </div>
